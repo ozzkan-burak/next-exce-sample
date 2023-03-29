eb9e5a6568fafca1cbf20d4a92c900c5f1d34300
ozzkan-burak <brk.ozzkan@gmail.com>
2023-03-29 23:46:21 +0300

    episode1

---
+++ src/pages/aboutUs.js
@@ -0,0 +1,14 @@
+import styles from '../styles/Home.module.css';
+
+const AboutUs = () => {
+  return(
+    <div className={styles.container}>
+      <div className={styles.title}>Burak's </div>
+      <p className={styles.description}>
+        Lorem Ipsum dolor sit amet.
+      </p>
+    </div>
+  );
+};
+
+export default AboutUs;
